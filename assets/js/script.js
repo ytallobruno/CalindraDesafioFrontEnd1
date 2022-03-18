aec9cda4e12eedf1aa8be95bf0399ee076e99763
Ytallo Bruno <ytallobruno@hotmail.com>
2022-03-18 11:28:24 -0300

    add async function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,20 @@ const btn = document.getElementById("btn");
 const caixaPesquisa = document.getElementById("pesquisar");
 const listaDeProdutos = document.getElementById("listaProdutos");
 
+btn.addEventListener("click", async function () {
+  const valor = caixaPesquisa.value;
+  if (valor.length > 0) {
+    const resposta = await fetch(
+      `https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=${valor}&source=nanook`
+    );
+    let productsList = await resposta.json();
+    products = productsList.products;
+
+    const lista = products.map((item) => `<h4>${item.name}</h4>`).join("");
+    listaDeProdutos.classList.add("mostrar");
+    listaDeProdutos.innerHTML = lista;
+  } else {
+    listaDeProdutos.innerHTML = "";
+    listaDeProdutos.classList.remove("mostrar");
+  }
+});
